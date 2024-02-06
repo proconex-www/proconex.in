68f92d1bdf4734707c71580d9340455dc1ac89e6
Proconex <proconexwebsite@gmail.com>
2024-02-06 16:35:32 +0530

    spare carousel added to major industries
    spare carousel added to major industries

--- js/proconEx-1.js
+++ js/proconEx-1.js
@@ -1,9 +1,6 @@
 // JavaScript Document of Proconex Pvt. Ltd.
 $(function(){
     $("#proconEx_bdy").addClass("fadein");
-	if($("#proconEx_carousel").length > 0){
-		
-	}
     if($(".product_photo_zoom").length > 0){
         $.getScript("js/jquery.zoom.js").done(function(){
             $(".product_photo_zoom").zoom({
@@ -12,37 +9,67 @@ $(function(){
             $(".product_photo_zoom").click(function(){
                 $(this).children(".icon").toggleClass("d-none");
             });
-//            $(".product_photo_zoom").on("touchstart", function(){
-//                $(this).children(".icon").toggleClass("d-none");
-//            });
-//            $(".product_photo_zoom").on("touchend", function(){
-//                $(this).children(".icon").toggleClass("d-none");
-//            })
         });
     }
     if($(".owl-carousel").length > 0){
         $.getScript("js/owl.carousel.js").done(function(){
-            $("#product_carousel").owlCarousel({
-                items:1,
-                autoplay:false,
-                loop:false,
-                center:true,
-                margin:10,
-                dots: false,
-                URLhashListener:true,
-                autoplayHoverPause:true,
-                startPosition: "URLHash"
-            });
-            $("#product_thumbs").owlCarousel({
-                items:4,
-                autoplay:false,
-                loop:false,
-                margin:10,
-                dots: false,
-                URLhashListener:true,
-                autoplayHoverPause:true,
-                startPosition: "URLHash"
-            });
+            if($("#proconEx_products").length > 0){
+                $owl = $("#proconEx_products");
+                $owl.owlCarousel({
+                    responsiveClass:true,
+                    responsive:{
+                        0:{
+                            items:1
+                        },
+                        576:{
+                            items:2
+                        },
+                        992:{
+                            items:3
+                        }
+                    },
+                    loop:false,
+                    center:false,
+                    margin:5,
+                    dots: false,
+                    autoplay:false
+                });
+                $(".product-next").click(function() {
+                    $owl.trigger("next.owl.carousel");
+                });
+                $(".product-prev").click(function() {
+                    $owl.trigger("prev.owl.carousel");
+                });
+//                if ( $owl.currentItem == 0 ) {
+//                  $(".product-next").addClass('disabled');
+//                }
+//                if ( $owl.currentItem == $owl.maximumItem ) {
+//                  $(".product-prev").addClass('disabled');
+//                }
+            }
+            if($("#product_carousel").length > 0){
+                $("#product_carousel").owlCarousel({
+                    items:1,
+                    autoplay:false,
+                    loop:false,
+                    center:true,
+                    margin:10,
+                    dots: false,
+                    URLhashListener:true,
+                    startPosition: "URLHash"
+                });
+            }
+            if($("#product_thumbs").length > 0){
+                $("#product_thumbs").owlCarousel({
+                    items:4,
+                    autoplay:false,
+                    loop:false,
+                    margin:10,
+                    dots: false,
+                    URLhashListener:true,
+                    startPosition: "URLHash"
+                });
+            }
         });
     }
     if($("input.ps_search").length > 0){
